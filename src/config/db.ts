3fe291e686dd05e10ae3b395aa84d57c5b793272
ShamardinaEM <shamardina.2005@gmail.com>
2025-12-15 22:27:23 +0600

    fix

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         const uri =
-            process.env.DATABASE_URL ||
-            "mongodb://localhost:27017/online-courses";
+            process.env.DATABASE_URL || "mongodb://mongo:27017/online-courses";
         await mongoose.connect(uri);
         console.log("MongoDB connected");
     } catch (err) {
